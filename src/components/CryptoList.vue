92759f2e7a7da3f9d5e6e9fca70f6a6169736058
OussamaAlouat <al317257@uji.es>
2022-01-28 09:51:37 +0100

    Add new component (CryptoList) to list all cards

---
+++ src/components/CryptoList.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <div v-for="crypto in cryptos" :key="crypto.id">
+      <crypto-card
+        :name="crypto.name"
+        :sku="crypto.sku"
+        :stock="crypto.stock"
+        :variant="crypto.variant"
+      ></crypto-card>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex';
+
+import CryptoCard from './CryptoCard.vue';
+
+export default {
+  components: { CryptoCard },
+  name: 'CriptoList',
+  computed: {
+    ...mapState(['cryptos']),
+  },
+};
+</script>
+
+<style>
+
+</style>
