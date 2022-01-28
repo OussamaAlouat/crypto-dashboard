6a1677a726ce0912e33940a51ee3cc365df63524
OussamaAlouat <al317257@uji.es>
2022-01-28 08:26:17 +0100

    WP: Crypto component, improved

--- src/components/CryptoCard.vue
+++ src/components/CryptoCard.vue
@@ -1,14 +1,17 @@
 <template>
   <div class="content">
-    <img class="crypto-img" />
-    <div class="crypto-container align-start">
-      <span>Cripto</span>
-      <span>Description</span>
+    <div class="align-start crypto-details">
+      <div class="crypto-img-container"><img class="crypto-img" /></div>
+      <div class="crypto-container">
+        <span class="crypto-name">Bitcoin</span>
+        <span class="crypto-short-name">BTC</span>
+      </div>
+
     </div>
 
     <div class="crypto-container align-end">
       <span>$38.729</span>
-      <span>+1,34%</span>
+      <span class="crypto-percentage">+1,34%</span>
     </div>
   </div>
 </template>
@@ -19,8 +22,30 @@ export default {
 </script>
 
 <style>
+.crypto-percentage{
+  font-size: var(--text-3);
+  color: var(--main-green)
+}
+
+.crypto-short-name {
+  color: var(--main-gray);
+  font-weight: 400;
+}
+.crypto-name {
+  font-weight: 700;
+  font-size: var(--text-2);
+}
+
+.crypto-img-container {
+  padding-right: 2rem;
+}
+.crypto-details {
+  display: flex;
+  flex-direction: row;
+}
+
 .crypto-img {
-  background: var(--main-blue);
+  background: var(--main-gray);
   width: 50px;
   height: 50px;
   border-radius: 50%;
