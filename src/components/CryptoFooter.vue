cd66e44a6967cc76e5f101bab9a0a2d1365b8476
OussamaAlouat <al317257@uji.es>
2022-01-31 10:13:58 +0100

    Add a crypto footer component

---
+++ src/components/CryptoFooter.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="icon-container">
+    <div class="icon-box">
+      <i class="k-icon-dashboard"></i>
+    </div>
+
+    <div class="icon-box">
+      <i class="k-icon-card"></i>
+    </div>
+
+    <div class="icon-box">
+        <i class=" k-icon-refresh"></i>
+    </div>
+
+    <div class="icon-box">
+      <i class="k-icon-settings"></i>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+};
+</script>
+
+<style lang="scss" scoped>
+.icon-box {
+  height: 64px;
+  width: 64px;
+  background: var(--main-blue);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.icon-container {
+  display: flex;
+  justify-content: space-evenly;
+}
+i {
+  color: var(--main-white);
+  size: 32px;
+}
+
+</style>
